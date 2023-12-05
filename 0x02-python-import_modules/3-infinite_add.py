93e43cc094b8a4e7a71118bc45c2c6f6746c55b9
EdwardDawn46 <edwarddawn31@gmail.com>
2023-12-05 11:18:19 -0800

    0x02 done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+def add_all(*args):
+    total = 0
+    for arg in args:
+        total += int(arg)
+    print(total)
